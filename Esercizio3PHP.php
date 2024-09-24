11a5accb95b67db532f8f9358877359886b47ea1
Gabriel <gabrielsiano2006@gmail.com>
2024-09-24 12:23:17 +0400

    commit 1

---
+++ Esercizio3PHP.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ESERCIZIO 3</title>
+</head>
+<body>
+    
+    <?php
+
+    $nRandom = rand(16, 60);
+
+    echo "<p style='font-size: " . $nRandom . "px'>Questo e' un paragrafo</p>";
+
+
+
+
+
+
+    ?>
+
+</body>
+</html>
